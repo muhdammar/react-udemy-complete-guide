9c0dee169662d5b9b2a32d1ed47aa52043de2803
muhdammar <ammar.aziz.work@gmail.com>
2026-05-20 17:57:07 +0800

    #Lifting state up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
-import classes from './PostsList.module.css'
-function PostsList(){
-    return <ul className={classes.posts} >
-      <Post author="Ammar" body="Course Financial"/>
-    </ul>
+import classes from "./PostsList.module.css";
+function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+      </ul>
+    </>
+  );
 }
-export default PostsList
+export default PostsList;
