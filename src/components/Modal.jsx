9e56fce3ad546acf95e3af3e6efa4217a3d49a9b
muhdammar <ammar.aziz.work@gmail.com>
2026-05-20 18:31:21 +0800

    #621 State & conditional content

---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import classes from "./Modal.module.css";
+function Modal({onClose,children}) {
+  return (
+    <>
+      <div className={classes.backdrop} onClick={onClose} />
+      <dialog open className={classes.modal}>{children}</dialog>
+    </>
+  );
+}
+
+export default Modal;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import Modal from "./Modal"
 function PostsList() {
+  const [ modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+  
+  function hideModalHandler(){
+    setModalIsVisible(false)
+  }
+  
   function changeBodyHandler(event) {
     setEnteredBody(event.target.value);
   }
@@ -14,7 +21,12 @@ function PostsList() {
 
   return (
     <>
-      <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+    {modalIsVisible ? <Modal onClose={hideModalHandler}>
+      <NewPost 
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler} />
+    </Modal> : null}
+    
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
       </ul>
