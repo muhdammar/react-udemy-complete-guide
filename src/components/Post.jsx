5dd5baf65d95c792962cbc34bb582e823ac0dc92
muhdammar <ammar.aziz.work@gmail.com>
2026-05-20 12:47:02 +0800

    # CSS Styling & CSS Modules

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,9 @@
+import styles from './Post.module.css'
 function Post(props) {
   return (
-    <div>
-      <p>{props.author}</p>
-      <p>{props.body}</p>
+    <div className={styles.post}>
+      <p className={styles.author}>{props.author}</p>
+      <p className={styles.text}>{props.body}</p>
     </div>
   );
 }
