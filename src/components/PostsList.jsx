6397bb4da2af9311aa95810551fe401c2817b780
muhdammar <ammar.aziz.work@gmail.com>
2026-05-21 18:35:25 +0800

    Outputting List Data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,25 +2,31 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import Modal from "./Modal"
+import Modal from "./Modal";
 
-function PostsList({isPosting, onStopPosting}) {
+function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
-  function addPostHandler(postData){
-    //setPosts([postData, ...posts]) try this
-    setPosts((existingPosts) => [[postData, ...existingPosts]])
+  function addPostHandler(postData) {
+    //setPosts([postData, ...posts]) //what different tho, later ask AI
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
     <>
-    {isPosting ? <Modal onClose={onStopPosting}>
-      <NewPost 
-        onCancel={onStopPosting}
-        onAddPost={addPostHandler}/>
-    </Modal> : null}
-    
-      <ul className={classes.posts}>
-          <Post />
-      </ul>
+      {isPosting ? (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      ) : null}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div> No data la bro </div>
+      )}
     </>
   );
 }
